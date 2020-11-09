ceb16c417705e7a206102e00bdf3c2e92636fa6c
vertexpotato <abdallaoday7@gmail.com>
2020-11-09 18:22:24 +0300

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.cpp",
+        "include/spdlog/**.h",
+    }
+
+    includedirs
+    {
+        "include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
