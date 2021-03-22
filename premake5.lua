51e8557d2b951479210d043165f3c7904830cb40
vertexpotato <abdallaoday7@gmail.com>
2021-03-22 18:24:42 +0300

    added premake5

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "off"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.cpp",
+        "include/spdlog/**.h",
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    defines {"SPDLOG_COMPILED_LIB"}
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
